b69bdd7da5242d540f61105ec89fe3ac29054579
Daniel Incicau <daniel_incicau@yahoo.com>
2024-04-23 15:35:56 +0200

    Update Starling.ipynb and STARLING_run.py

--- STARLING_run.py
+++ STARLING_run.py
@@ -5,6 +5,8 @@ import argparse
 import anndata as ad
 import pandas as pd
 import numpy as np
+import scanpy as sc
+
 import pytorch_lightning as pl
 from lightning_lite import seed_everything
 from pytorch_lightning.callbacks import EarlyStopping
@@ -17,6 +19,14 @@ COLUMNS_OF_INTEREST = ['sample_id', 'object_id', 'cell_type', 'init_label',
                        'st_label', 'doublet', 'doublet_prob', 'max_assign_prob']
 
 UNLABELED_CELL_TYPES = ['unlabeled', 'undefined', 'unknown', 'BnTcell', "BnT cell"]
+
+
+def _plot_umap(adata):
+    sc.pp.neighbors(adata)
+    sc.tl.umap(adata)
+    fig = sc.pl.umap(adata, color=['st_pred'], size=5, return_fig=True)
+
+    return fig
 
 
 def main():
@@ -39,19 +49,8 @@ def main():
 
     seed_everything(args.seed, workers=True)
 
-    # Prepare dataset
-    dataset_df = pd.read_csv(args.dataset)
-    dataset_df['rownames'] = dataset_df['sample_id'].astype(str) + '_' + dataset_df['object_id'].astype(str)
-    dataset_df.set_index('rownames', inplace=True)
-
-    interest_columns = ['image_name', 'sample_id', 'object_id', 'cell_type', 'patient_id', 'area', 'x', 'y', 'user_init_label']
-    X_columns = [c for c in dataset_df.columns if c not in interest_columns]
-    obs_columns = [c for c in dataset_df.columns if c in interest_columns]
-    obs_df = dataset_df[obs_columns]
-    X_df = dataset_df[X_columns]
-
-    adata = ad.AnnData(X=X_df.values, obs=obs_df)
-    adata.obs_names = obs_df.index
+    # Load data
+    adata = ad.read_h5ad(args.dataset)
 
     # Annotate initial clustering with KM clustering results
     print(f'Initial cluster annotation using `{args.init_clustering_method}` algorithm.')
@@ -69,12 +68,9 @@ def main():
     assert "init_label" in adata.obs
     assert adata.obs["init_label"].shape == (adata.X.shape[0],)
 
-    labeled_obs = adata.obs[~adata.obs['cell_type'].isin(UNLABELED_CELL_TYPES)]
-    print("Init ARI:", adjusted_rand_score(labeled_obs['cell_type'], labeled_obs['init_label']))
-
     # Setting initializations
     st = starling.ST(adata, learning_rate=args.lr, singlet_prop=args.error_free_cells_prop)
-    print(st.singlet_prop)
+
     log_tb = TensorBoardLogger(save_dir="log")
     cb_early_stopping = EarlyStopping(monitor="train_loss", mode="min", verbose=False)
     trainer = pl.Trainer(
@@ -99,24 +95,32 @@ def main():
     # map st label to cell type
     st.adata.obs['st_label'] = mapper.get_pred_labels(st.adata.obs['st_label'])
 
-    st.labeled_obs = st.adata.obs[~st.adata.obs['cell_type'].isin(UNLABELED_CELL_TYPES)]
-    print("Starling ARI:", adjusted_rand_score(st.labeled_obs['cell_type'], st.labeled_obs['st_label']))
-
-    # Save annotated dataset
-    adata.write(filename=os.path.join(args.base_path, 'starling_anndata.h5ad'))
-
-    # Save results csv
-    results_df = adata.obs[COLUMNS_OF_INTEREST]
-
     prob_matrix = adata.obsm['assignment_prob_matrix']
     prob_vector = np.array([f"[{', '.join(map(str, row))}]" for row in prob_matrix])
-    results_df['prob_list'] = prob_vector
+    adata.obs['st_prob_list'] = prob_vector
 
-    # rename columns
-    results_df.columns = ['image_id', 'cell_id', 'label', 'init_pred', 'st_pred', 'doublet', 'doubled_prob',
-                          'st_pred_prob', 'st_prob_list']
+    results_df = adata.obs[COLUMNS_OF_INTEREST]
+    results_df = results_df.rename(columns={
+        'sample_id': 'image_id',
+        'object_id': 'cell_id',
+        'cell_type': 'label',
+        'init_label': 'init_pred',
+        'st_label': 'st_pred',
+        'max_assign_prob': 'st_pred_prob'
+    })
 
+    # Save results csv
     results_df.to_csv(os.path.join(args.base_path, 'starling_results.csv'), index=False)
+
+    # Plot UMAP of predictions
+    figure = _plot_umap(adata)
+    figure.savefig(os.path.join(args.base_path, 'UMAP_predictions.pdf'), format="pdf", bbox_inches="tight")
+
+    # Calculate ARI Score of labeled results
+    labeled_results_df = results_df[~results_df['cell_type'].isin(UNLABELED_CELL_TYPES)]
+
+    print("Init ARI:", adjusted_rand_score(labeled_results_df['label'], labeled_results_df['init_pred']))
+    print("Starling ARI:", adjusted_rand_score(labeled_results_df['label'], labeled_results_df['st_pred']))
 
 
 if __name__ == '__main__':
